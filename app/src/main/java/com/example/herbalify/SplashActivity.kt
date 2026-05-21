fbd5c45764d608103fbf31689874b94c776de8a3
salsareike40@gmail.com <salsareike40@gmail.com>
2026-05-21 11:50:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/herbalify/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.herbalify
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val btnMulai = findViewById<Button>(R.id.btnMulai)
+        val txtMasuk = findViewById<TextView>(R.id.txtMasuk)
+
+        btnMulai.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        txtMasuk.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
